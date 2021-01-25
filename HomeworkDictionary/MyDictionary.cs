90df3a7c011226b83b20a165d2340aa6f3631fe8
Osman Bal <190402015@ogr.gelisim.edu.tr>
2021-01-25 23:49:48 +0300

    Add project files.

---
+++ HomeworkDictionary/MyDictionary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<A, B>
+    {
+        A[] items1;
+        B[] items2;
+        public MyDictionary()
+        {
+            items1 = new A[0];
+            items2 = new B[0];
+        }
+        public void Add(A item1, B item2)
+        {
+            A[] tempArray1 = items1;
+            B[] tempArray2 = items2;
+            items1 = new A[items1.Length + 1];
+            items2 = new B[items2.Length + 1];
+            for (int i = 0; i < tempArray1.Length; i++)
+            {
+                items1[i] = tempArray1[i];
+            }
+            items1[items1.Length - 1] = item1;
+
+            for (int j = 0; j < tempArray2.Length; j++)
+            {
+                items2[j] = tempArray2[j];
+            }
+            items2[items2.Length - 1] = item2;
+        }
+
+
+        public int Count
+        {
+            get { return items1.Length; }
+        }
+
+        public int Count2
+        {
+            get { return items2.Length; }
+        }
+    }
+}
+
---
+++ HomeworkDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> ürünler = new MyDictionary<int, string>(); //sırayla ürünün ID'sini ve adını tanımlıyoruz.
+            ürünler.Add(1, "Arduino");
+            ürünler.Add(2, "RFID");
+            ürünler.Add(3, "Raspberry Pi");
+            ürünler.Add(4, "Potansiyometre");
+            ürünler.Add(5, "DC Motor");
+            ürünler.Add(6, "Transistör");
+            Console.WriteLine(ürünler.Count + ürünler.Count2);
+
+        }
+
+    }
+}
